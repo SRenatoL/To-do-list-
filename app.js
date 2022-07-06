a41166d790fc3ae3b972465d45682982bb0ef160
SRenatoL <silva.renat136@gmail.com>
2022-07-06 10:03:55 -0300

    Adição do nodemon e testes no localhost

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>My To do list :)</h1>')
+})
+
+app.listen(3000, () => {
+    console.log('Server has been started')
+})
