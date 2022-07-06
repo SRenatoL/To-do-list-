8170866bdf49afc8eb36c70f5a37061acfd73c49
SRenatoL <silva.renat136@gmail.com>
2022-07-06 18:20:39 -0300

    atualização do Middleware

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let day = date.getDate()
 let mouth = date.getMonth()
 let year = date.getFullYear()
 
-
 const app = express();
 
 app.use(express.json());
@@ -13,7 +12,8 @@ app.use(express.json());
 const log = (req, res, next) => {
     console.log(req.body)
     console.log(`Date: ${day}/${(mouth + 1)}/${year}`)
-}
+    next()
+  }
 
 app.use(log)
 
@@ -21,11 +21,13 @@ app.get("/", (req, res) => {
   res.send("<h1>My To do list :)</h1>");
 });
 
+app.listen(3000, () => {
+  console.log("Server has been started");
+});
+
 app.get("/json", (req, res) => {
   console.log(req.body);
   res.json({ title: "Task X", done: true });
 });
 
-app.listen(3000, () => {
-  console.log("Server has been started");
-});
+
