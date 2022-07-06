3328757053731cac7529cff779f3ca226fb95908
SRenatoL <silva.renat136@gmail.com>
2022-07-06 10:16:42 -0300

    Testes de utilização do json com express/nodemon

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('<h1>My To do list :)</h1>')
 })
 
+app.get('/json', (req, res) => {
+    res.json({title: 'Task X', done: true})
+})
+
 app.listen(3000, () => {
     console.log('Server has been started')
 })
