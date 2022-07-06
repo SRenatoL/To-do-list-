d04b5c3fe426835575c48611059d6dba6ef61030
SRenatoL <silva.renat136@gmail.com>
2022-07-06 12:04:27 -0300

    criação de Middlewares e testes com o postman

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express = require('express');
+const express = require("express");
+const date = new Date
 
-const app = express()
+let day = date.getDate()
+let mouth = date.getMonth()
+let year = date.getFullYear()
 
-app.get('/', (req, res) => {
-    res.send('<h1>My To do list :)</h1>')
-})
 
-app.get('/json', (req, res) => {
-    res.json({title: 'Task X', done: true})
-})
+const app = express();
+
+app.use(express.json());
+
+const log = (req, res, next) => {
+    console.log(req.body)
+    console.log(`Date: ${day}/${(mouth + 1)}/${year}`)
+}
+
+app.use(log)
+
+app.get("/", (req, res) => {
+  res.send("<h1>My To do list :)</h1>");
+});
+
+app.get("/json", (req, res) => {
+  console.log(req.body);
+  res.json({ title: "Task X", done: true });
+});
 
 app.listen(3000, () => {
-    console.log('Server has been started')
-})
+  console.log("Server has been started");
+});
