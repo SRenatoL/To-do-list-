976d49a185dd3eb284a2e525255ab23f89e51cc8
SRenatoL <silva.renat136@gmail.com>
2022-07-06 21:58:53 -0300

    criação/teste de rotas

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
 const express = require("express");
-const date = new Date
-
-let day = date.getDate()
-let mouth = date.getMonth()
-let year = date.getFullYear()
+const checkListRouter = require('./src/routes/checklist')
 
 const app = express();
+app.use(express.json()) //express.json sempre em cima :D
 
-app.use(express.json());
-
-const log = (req, res, next) => {
-    console.log(req.body)
-    console.log(`Date: ${day}/${(mouth + 1)}/${year}`)
-    next()
-  }
-
-app.use(log)
-
-app.get("/", (req, res) => {
-  res.send("<h1>My To do list :)</h1>");
-});
+app.use('/checklists',checkListRouter);
 
 app.listen(3000, () => {
   console.log("Server has been started");
 });
 
-app.get("/json", (req, res) => {
-  console.log(req.body);
-  res.json({ title: "Task X", done: true });
-});
 
 
---
+++ src/routes/checklist.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('Hello');
+  res.send();
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.body);
+})
+
+
+module.exports = router;
