79928b06798061833bb122c8eca4772adc9720a3
SRenatoL <silva.renat136@gmail.com>
2022-07-06 22:18:00 -0300

    teste verbos express

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -12,5 +12,20 @@ router.post('/', (req, res) => {
   res.status(200).json(req.body);
 })
 
+router.get('/:id', (req, res) => {
+    console.log(req.body)
+    res.send(`ID: ${req.params.id}`)
+})
+
+router.put('/:id', (req, res) => {
+    console.log(req.body)
+    res.send(`PUT ID: ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    console.log(req.body)
+    res.send(`DELETE ID: ${req.params.id}`)
+})
+
 
 module.exports = router;
